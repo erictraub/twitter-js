86f6700d5abdc85202bb95dbc298508cd70f8ec0
Eric Traub <est2114@columbia.edu>
2016-03-08 14:16:52 -0500

    updates

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
 
+app.use(function(request, response, next){
+	console.log(request.method, request.originalUrl, response.statusCode);
+	next();
+})
+
+app.use("/special", function(request, response){
+	response.send(' ')
+	console.log('you reached the special area')
+})
+
+
 app.get("/", function(request, response){
 	response.send('Welcome Message	 	')
 })
 app.get('/news', function(request, response){
 	response.send('News Here')
 })
+
+
 app.listen(3000);
 console.log('server listening...');
